cbb8a2eaf728d3cc0444a7ee123bd21b85b4cfb7
rafael <rcm2005@icloud.com>
2023-05-22 17:10:20 -0300

    adicionando exercicio 6, ferramenta de adição de notas

--- exercicio 6.py
+++ exercicio 6.py
@@ -1,2 +1,14 @@
 lista = []
 
+nota = 1.0
+
+while nota >= 0:
+    nota = float(input("digite o valor da nota: "))
+    if nota >= 0:
+        lista.append(nota)
+    
+    
+
+print (lista)
+
+    
